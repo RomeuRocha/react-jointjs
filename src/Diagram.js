0f20f73fdfcd93f7e66746a1f121a68ea9185ab6
RomeuRocha <romeu.d.rocha@gmail.com>
2021-09-05 17:54:08 -0300

    first commit

---
+++ src/Diagram.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react';
+import /** as*/ joint from 'jointjs'
+
+//import Shape from './Shape'
+
+
+const Diagram = () => {
+
+
+    const canvas = useRef();
+
+    useEffect(() => {
+        let graph = new joint.dia.Graph();
+
+        console.log(canvas.current)
+
+        var paper = new joint.dia.Paper({
+          el: canvas.current,
+          model: graph,
+          width: 1000,
+          height: 600,
+          gridSize: 1
+      });
+  
+        var rect = new joint.shapes.standard.Rectangle({});
+        rect.position(100, 30);
+        rect.resize(100, 40);
+        rect.attr({
+            body: {
+                fill: 'orange'
+            },
+            label: {
+                text: 'Hello',
+                fill: 'white'
+            }
+        });
+        rect.addTo(graph);
+
+        var rect2 = rect.clone();
+        rect2.translate(300, 0);
+        rect2.attr('label/text', 'World!');
+        rect2.addTo(graph);
+
+        var link = new joint.shapes.standard.Link();
+        link.source(rect);
+        link.target(rect2);
+        link.addTo(graph);
+
+    }, []);
+
+    return (
+        <div className="canvas" ref={canvas} />
+    );
+}
+
+export default Diagram;
---
+++ src/Shape.js
@@ -0,0 +1,31 @@
+import * as joint from 'jointjs'
+
+
+
+joint.dia.Element.define('standard.Rectangle', {
+    attrs: {
+        body: {
+            refWidth: '100%',
+            refHeight: '100%',
+            strokeWidth: 2,
+            stroke: '#000000',
+            fill: '#FFFFFF'
+        },
+        label: {
+            textVerticalAnchor: 'middle',
+            textAnchor: 'middle',
+            refX: '50%',
+            refY: '50%',
+            fontSize: 14,
+            fill: '#333333'
+        }
+    }
+}, {
+    markup: [{
+        tagName: 'rect',
+        selector: 'body',
+    }, {
+        tagName: 'text',
+        selector: 'label'
+    }]
+});
